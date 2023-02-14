c9961a844da447789986fdcfc6a47e3bf208b92a
johnkingkong <jeanre002@gmail.com>
2023-02-14 13:31:48 -0500

    fixed

--- srcs/requirements/mariadb/tools/setUpDB.sh
+++ srcs/requirements/mariadb/tools/setUpDB.sh
@@ -1,32 +1,36 @@
 FILE=/home/jvigneau/data/mariadb/success
 
-if test -f "/home/jvigneau/data/mariadb/success"; then
-	echo "DB already set up"
-	exec $@
+if test -f "/home/jvigneau/data/mariadb/success" ; then
+	echo "DATABASE ALREADY SET UP"
 else
-	echo "Setting up the DB"
+	echo "SETUPING DATABASE"
 	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
 	mysqld --user=mysql --datadir=/var/lib/mysql &
 	sleep 5
 
 	mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_DB};"
-	echo "DB created"
 
-	mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFED BY '${DB_USERPASS}';"
-	echo "User created"
+	echo "CREATED DATABASE"
 
-	mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DB}\`.* TO \`${DB_USER}\`@'%' IDENTIFED BY '${DB_USERPASS}';"
-	echo "Granted privileges"
+	mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_USERPASS}';"
 
-	mysql -e "ALTER USE 'root'@'localhost' IDENTIFED BY '${DB_ROOTPASS}';"
-	echo "Root password is now set"
+	echo "CREATED USER"
+
+	mysql -e "GRANT ALL PRIVILEGES ON \`${WP_DB}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_USERPASS}';"
+
+	echo "PRIVILEGES GRANTED"
+
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOTPASS}';"
+
+	echo "ROOT PASSWORD SET"
 
 	mysql -u root -p${DB_ROOTPASS} -e "FLUSH PRIVILEGES;"
-	echo "Done"
+
+	echo "PRIVILEGES FLUSHED"
 
 	touch /home/jvigneau/data/mariadb/success
-	pkill myslqd
+	pkill mysqld
 
-	exec $@
 fi
+exec $@
