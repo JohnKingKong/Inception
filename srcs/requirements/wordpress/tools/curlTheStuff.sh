55b5a0ccff9118baf674f1d56f0af7549576c564
johnkingkong <jeanre002@gmail.com>
2023-02-14 13:58:57 -0500

    fixed

--- srcs/requirements/wordpress/tools/curlTheStuff.sh
+++ srcs/requirements/wordpress/tools/curlTheStuff.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!bin/bash
 
 set -exo pipefail
 
@@ -13,37 +13,42 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
 	if [ "$1" = "php-fpm7" ]; then
 
-		for i in {0..42}; do
+		for i in {0..30}; do
 			if mariadb -h$DB_SERV -u$DB_USER -p$DB_USERPASS --database=$WP_DB <<< 'SELECT 1;' &> /dev/null; then
 				break
 			fi
+			sleep 2
 		done
 	fi
 
-	wp config create --allow-root \
-		--dbname=$WP_DB \
-		--dbuser=$DB_USER \
-		--dbpass=$DB_USERPASS \
-		--dbhost=$DB_SERV \
-		--dbcharset="utf8" \
-		--dbcollate="utf8_general_ci" \
-		--path="/var/www/html"
 
-	wp core install --allow-root \
-		--title="${WP_TITLE}" \
-		--admin_name="${WP_ADMIN}" \
-		--admin_password="${WP_ADMINPASS}" \
-		--admin_email="${WP_ADMINEMAIL}" \
-		--skip-email \
-		--url="${SERV_NAME}" \
-		--path="/var/www/html"
+		wp config create \
+			--allow-root \
+			--dbname=$WP_DB \
+			--dbuser=$DB_USER \
+			--dbpass=$DB_USERPASS \
+			--dbhost=$DB_SERV \
+			--dbcharset="utf8" \
+			--dbcollate="utf8_general_ci" \
+			--path="/var/www/html"
 
-	wp user create --allow-root \
-		$WP_USER \
-		$WP_USEREMAIL \
-		--role=author \
-		--user_pass=$WP_USERPASS \
-		--path="/var/www/html"
+		wp 	core install \
+			--allow-root \
+			--title="${WP_TITLE}" \
+			--admin_name="${DB_USER}" \
+			--admin_password="${DB_USERPASS}" \
+			--admin_email="wordpress@superuser.com" \
+			--skip-email \
+			--url="${SERV_NAME}" \
+			--path="/var/www/html" 
+
+		wp 	user create \
+			--allow-root \
+			$WP_USER \
+			$WP_USEREMAIL \
+			--role=author \
+			--user_pass=$WP_USERPASS \
+			--path="/var/www/html" 
 
 fi
 
