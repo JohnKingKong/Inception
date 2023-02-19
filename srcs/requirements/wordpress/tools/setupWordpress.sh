c6ed4122a7bb504fa18df2470b9bb3d145fbaae4
Jean-rene Vigneault <jvigneau@42quebec.com>
2023-02-19 13:38:40 -0500

    fixed

--- srcs/requirements/wordpress/tools/setupWordpress.sh
+++ srcs/requirements/wordpress/tools/setupWordpress.sh
@@ -11,14 +11,12 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
 	cp ./www.conf /etc/php8/php-fpm.d/www.conf
 
-	if [ "$1" = "php-fpm8" ]; then
 		for i in {0..42}; do
 			if mariadb -h$DB_SERVER -u$DB_USER -p$DB_USERPASS --database=$WORDPRESS_DB <<< 'SELECT 1;' &> /dev/null; then
 				break
 			fi
 			sleep 2
 		done
-	fi
 
 	wp config create --allow-root --dbname=$WORDPRESS_DB --dbuser=$DB_USER --dbpass=$DB_USERPASS \
 		--dbhost=$DB_SERVER --dbcharset="utf8" --dbcollate="utf8_general_ci" --path="/var/www/html"
